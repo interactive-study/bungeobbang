0b877fb55f2dbeb930e6c66abc9eaec952692c61
Minkyu Lee <whizkyu@snu.ac.kr>
2025-03-28 22:26:36 +0900

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/globals.css';
-import App from './App.tsx';
+import BungeobbangSlides from './pages/BungeobbangSlides.tsx';
+import BungeobbangMold from './pages/BungeobbangMold.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to={'/slides'} />,
+  },
+  {
+    path: '/slides',
+    Component: BungeobbangSlides,
+  },
+  {
+    path: '/mold',
+    Component: BungeobbangMold,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ src/pages/BungeobbangMold.tsx
@@ -0,0 +1,3 @@
+export default function BungeobbangMold() {
+    return <div></div>
+}
