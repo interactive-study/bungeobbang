aa57a79237c4e612fcf2f8c415b1b84f44cc0d3a
Minkyu Lee <whizkyu@snu.ac.kr>
2025-04-02 17:50:27 +0900

    start 누르면 시작

--- src/pages/maker/index.tsx
+++ src/pages/maker/index.tsx
@@ -1,7 +1,32 @@
-import styles from './styles.module.css';
 import Bungeo from '@/assets/Bungeo.png';
+import { useCallback, useEffect, useState } from 'react';
+import styles from './styles.module.css';
+
+import head1 from '@/assets/temp/head1.png';
+import head2 from '@/assets/temp/head2.png';
+import head3 from '@/assets/temp/head3.png';
+import head4 from '@/assets/temp/head4.png';
 
 export default function BungeobbangMaker() {
+  const [isStarted, setIsStarted] = useState(false);
+
+  const handleStart = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.code === 'Space' && !isStarted) {
+        e.preventDefault();
+        setIsStarted(true);
+      }
+    },
+    [isStarted]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleStart);
+    return () => {
+      window.removeEventListener('keydown', handleStart);
+    };
+  }, [handleStart]);
+
   return (
     <main>
       <div className={styles.page}>
@@ -12,15 +37,34 @@ export default function BungeobbangMaker() {
         </div>
 
         <div className={styles.frame}>
-          <div className={styles.frame__upper} />
-          <div className={styles.frame__lower} />
-          <div className={styles.frame__modal}>
-            Press space to start
+          <div className={styles.frame__upper}>
+            <div
+              className={`${styles.select} ${isStarted ? styles.fadeLeft : ''}`}
+            >
+            <div className={styles.select__option}>
+              <img src={head1} alt="" />
+            </div>
+              <div className={styles.select__option}>
+                <img src={head2} alt="" />
+              </div>
+              <div className={styles.select__option}>
+                <img src={head3} alt="" />
+              </div>
+              <div className={styles.select__option}>
+                <img src={head4} alt="" />
+              </div>
+            </div>
           </div>
+          <div className={styles.frame__lower} />
+          {isStarted || (
+            <div className={styles.frame__modal}>Press space to start</div>
+          )}
         </div>
 
         <div className={styles.rail}>
-          <div className={styles.rail__bungeo}>
+          <div
+            className={`${styles.rail__bungeo} ${isStarted ? styles.fadeLeft : ''}`}
+          >
             <img className={styles.rail__bungeoImg} alt="" src={Bungeo} />
           </div>
         </div>
